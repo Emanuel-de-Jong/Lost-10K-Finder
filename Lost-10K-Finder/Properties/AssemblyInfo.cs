f11f0200e2f6f61507a4a65c5d7985009c9214e5
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-04-09 16:36:12 +0200

    Framework change final steps

--- Lost-10K-Finder/Properties/AssemblyInfo.cs
+++ Lost-10K-Finder/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1665e170-6df3-4bf7-86b6-cc7252c1100d")]
+[assembly: Guid("7c0b46c6-eb8d-4f21-9576-84ab405da2c3")]
 
 // Version information for an assembly consists of the following four values:
 //
